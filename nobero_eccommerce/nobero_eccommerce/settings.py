8be962e001867bb62712371e44948f6f300d9967
SIMON-KANGI <simonmwangikangi@gmail.com>
2024-08-23 20:52:47 +0300

    settings

--- nobero_eccommerce/nobero_eccommerce/settings.py
+++ nobero_eccommerce/nobero_eccommerce/settings.py
@@ -3,6 +3,7 @@ import os
 import django
 from pathlib import Path
 
+from shutil import which
 # Path to your scraper directory
 SCRAPER_DIR = Path('/home/simon/nobero/nobero_api/scraper')
 if str(SCRAPER_DIR) not in sys.path:
@@ -29,53 +30,14 @@ SPIDER_MODULES = ["nobero_eccommerce.spiders"]
 NEWSPIDER_MODULE = "nobero_eccommerce.spiders"
 
 ROBOTSTXT_OBEY = True
+##selenium settings
+SELENIUM_DRIVER_NAME = 'chrome'
+SELENIUM_DRIVER_EXECUTABLE_PATH = which('chromedriver')
+SELENIUM_DRIVER_ARGUMENTS = ['--headless']  # '--headless' if using a headless browser
 
-# Configure maximum concurrent requests performed by Scrapy (default: 16)
-# CONCURRENT_REQUESTS = 32
-
-# Configure a delay for requests for the same website (default: 0)
-# DOWNLOAD_DELAY = 3
-
-# Disable cookies (enabled by default)
-# COOKIES_ENABLED = False
-
-# Disable Telnet Console (enabled by default)
-# TELNETCONSOLE_ENABLED = False
-
-# Override the default request headers
-# DEFAULT_REQUEST_HEADERS = {
-#     "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
-#     "Accept-Language": "en",
-# }
-
-# Enable or disable spider middlewares
-# SPIDER_MIDDLEWARES = {
-#     "nobero_eccommerce.middlewares.NoberoEccommerceSpiderMiddleware": 543,
-# }
-
-# Enable or disable downloader middlewares
-# DOWNLOADER_MIDDLEWARES = {
-#     "nobero_eccommerce.middlewares.NoberoEccommerceDownloaderMiddleware": 543,
-# }
-
-# Enable or disable extensions
-# EXTENSIONS = {
-#     "scrapy.extensions.telnet.TelnetConsole": None,
-# }
-
-# Enable and configure the AutoThrottle extension (disabled by default)
-# AUTOTHROTTLE_ENABLED = True
-# AUTOTHROTTLE_START_DELAY = 5
-# AUTOTHROTTLE_MAX_DELAY = 60
-# AUTOTHROTTLE_TARGET_CONCURRENCY = 1.0
-# AUTOTHROTTLE_DEBUG = False
-
-# Enable and configure HTTP caching (disabled by default)
-# HTTPCACHE_ENABLED = True
-# HTTPCACHE_EXPIRATION_SECS = 0
-# HTTPCACHE_DIR = "httpcache"
-# HTTPCACHE_IGNORE_HTTP_CODES = []
-# HTTPCACHE_STORAGE = "scrapy.extensions.httpcache.FilesystemCacheStorage"
+DOWNLOADER_MIDDLEWARES = {
+    'scrapy_selenium.SeleniumMiddleware': 800,
+}
 
 # Set settings whose default value is deprecated to a future-proof value
 REQUEST_FINGERPRINTER_IMPLEMENTATION = "2.7"
